f1921eb0a79d3833322c2d4bc7b1fca309156342
Matt Jones <al2o3cr@gmail.com>
2022-02-22 18:08:43 -0500

    minimal UI for creating rereleases

--- app/controllers/rereleases_controller.rb
+++ app/controllers/rereleases_controller.rb
@@ -1,9 +1,21 @@
 class RereleasesController < ApplicationController
+  def new
+    movie = Movie.find(params[:movie_id])
+
+    render locals: { movie: movie }
+  end
+
   def create
     movie = Movie.find(params[:movie_id])
 
-    rerelease = movie.rerelease(params)
+    rerelease = movie.rerelease(rerelease_params)
 
     redirect_to movie_path(rerelease)
+  end
+
+  private
+
+  def rerelease_params
+    params.require(:rerelease).permit(:year)
   end
 end
