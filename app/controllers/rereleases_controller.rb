9f362d8fae5950a53ac3570afd065dfa76ef82e4
Matt Jones <al2o3cr@gmail.com>
2022-02-23 13:58:33 -0500

    refactor to a rerelease object

--- app/controllers/rereleases_controller.rb
+++ app/controllers/rereleases_controller.rb
@@ -2,17 +2,19 @@ class RereleasesController < ApplicationController
   def new
     movie = Movie.find(params[:movie_id])
 
-    render locals: { movie: movie }
+    rerelease = Rerelease.new(movie, {})
+
+    render locals: { rerelease: rerelease }
   end
 
   def create
     movie = Movie.find(params[:movie_id])
 
-    rereleaser = Rereleaser.new(movie)
+    rerelease = Rerelease.new(movie, rerelease_params)
 
-    rerelease = rereleaser.run(rerelease_params)
+    rerelease.save
 
-    redirect_to movie_path(rerelease)
+    redirect_to movie_path(rerelease.id)
   end
 
   private
