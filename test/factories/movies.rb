147797d62ed4d5dad7484db1ded1be878e9bf5f3
Joe Dupuis <joe@dupuis.io>
2022-02-25 10:46:34 -0800

    Added stars and Directed by

--- test/factories/movies.rb
+++ test/factories/movies.rb
@@ -5,5 +5,6 @@ FactoryBot.define do
     facebook_likes { rand(100) }
     plot_keywords { "#{Faker::Lorem.word}|#{Faker::Lorem.word}"}
     year { Faker::Date.between(from: 10.years.ago, to: Date.today).year }
+    stars { rand(5) }
   end
 end
